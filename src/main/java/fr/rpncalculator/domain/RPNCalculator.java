a5bd8507b8fd1bc02a89bfa121d1da0a13a006b4
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-29 21:12:24 +0200

    Calculate 3 5 +

--- src/main/java/fr/rpncalculator/domain/RPNCalculator.java
+++ src/main/java/fr/rpncalculator/domain/RPNCalculator.java
@@ -1,8 +1,14 @@
 package fr.rpncalculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RPNCalculator implements Calculator {
   @Override
   public int calculate(String operation) {
+    List<String> operationElements = Arrays.asList(operation.split(" "));
+    if (operationElements.size() > 1)
+      return Integer.valueOf(operationElements.get(0)) + Integer.valueOf(operationElements.get(1));
     return Integer.valueOf(operation);
   }
 }
--- src/test/java/fr/rpncalculator/domain/RPNCalculatorTest.java
+++ src/test/java/fr/rpncalculator/domain/RPNCalculatorTest.java
@@ -1,13 +1,25 @@
 package fr.rpncalculator.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RPNCalculatorTest {
+  private Calculator calculator;
+
+  @Before
+  public void setUp() throws Exception {
+    calculator = new RPNCalculator();
+  }
+
   @Test
   public void should_return_same_value_if_there_is_only_one() throws Exception {
-    Calculator calculator = new RPNCalculator();
     assertThat(calculator.calculate("3")).isEqualTo(3);
+  }
+
+  @Test
+  public void should_calculate_8_when_adding_3_and_5() throws Exception {
+    assertThat(calculator.calculate("3 5 +")).isEqualTo(8);
   }
 }
